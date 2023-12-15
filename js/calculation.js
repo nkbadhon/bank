cde0dc68ed4ad810535f824acf419bf688b453c2
nkbadhon <badhon7574@gmail.com>
2023-12-15 15:11:50 +0600

    working

--- js/calculation.js
+++ js/calculation.js
@@ -1,1 +1,15 @@
-console.log('abc');
+function depositFunc() {
+    const deposit = document.getElementById('deposit');
+    const depositValue = deposit.value;
+    deposit.value = '';
+
+
+}
+
+const depositTotalFromPage = document.getElementById('deposit-total');
+const depositTotalstring = depositTotalFromPage.innerText;
+const depositTotalstringvalue = parseFloat(depositTotalstring)
+console.log(depositTotalstringvalue);
+// const depositTotal = parseFloat(depositTotalstring);
+// console.log(depositTotalstring);
+// console.log(depositTotalstring);
