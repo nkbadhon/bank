f24f631091e4809715f22a657d3155d97acf4235
nkbadhon <badhon7574@gmail.com>
2023-12-15 15:43:07 +0600

    calculation

--- js/calculation.js
+++ js/calculation.js
@@ -1,8 +1,22 @@
 function depositFunc() {
     const deposit = document.getElementById('deposit');
-    const depositValue = deposit.value;
+    const depositString = deposit.value;
     deposit.value = '';
+    const depositValue = parseFloat(depositString);
 
+    // getting the current value 
+    const depositTotalFromPage = document.getElementById('deposit-total');
+    const depositTotalstring = depositTotalFromPage.innerText;
+    const depositTotalstringvalue = parseFloat(depositTotalstring)
+    console.log(depositTotalstringvalue);
+    depositTotalFromPage.innerText = depositTotalstringvalue + depositValue;
+
+    // update in the current balance
+    const currentBalanceWhole = document.getElementById('balance-total');
+    const currentBalancetring = currentBalanceWhole.innerText;
+    const currentBalance = parseFloat(currentBalancetring);
+    const updatedBalance = currentBalance + depositValue;
+    currentBalanceWhole.innerText = updatedBalance;
 
 }
 
@@ -10,6 +24,7 @@ const depositTotalFromPage = document.getElementById('deposit-total');
 const depositTotalstring = depositTotalFromPage.innerText;
 const depositTotalstringvalue = parseFloat(depositTotalstring)
 console.log(depositTotalstringvalue);
-// const depositTotal = parseFloat(depositTotalstring);
-// console.log(depositTotalstring);
-// console.log(depositTotalstring);
+
+
+
+
